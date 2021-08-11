a6297af556833477d2df3a5611f0166dc7295f49
Red3ffect <red3ffect117@gmail.com>
2021-08-10 22:04:31 -0700

    Update/Clean up

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,13 +1,13 @@
 require('bootstrap');
 const createEl = require('./domMethods');
 
-const purchaseBtn = document.getElementById("purchaseBtn");
-const purchaseEmail = document.getElementById("purchaseEmail");
-const modalEl = document.querySelector(".modal-content");
-const modalBodyEl = document.querySelector(".modal-body");
-const modalFooterEl = document.querySelector(".modal-footer");
-
 if (window.location.href.indexOf("tickets") > -1) {
+    const purchaseBtn = document.getElementById("purchaseBtn");
+    const purchaseEmail = document.getElementById("purchaseEmail");
+    const modalEl = document.querySelector(".modal-content");
+    const modalBodyEl = document.querySelector(".modal-body");
+    const modalFooterEl = document.querySelector(".modal-footer");
+
     function purchaseTicket() {
 
         modalEl.removeChild(modalBodyEl)
@@ -18,7 +18,6 @@ if (window.location.href.indexOf("tickets") > -1) {
                 `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
             ),
         ))
-
     }
     purchaseBtn.addEventListener("click", purchaseTicket);
 };
